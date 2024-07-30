091efb8c260ea95f749fe0b8a7f598b9b783f0fe
sneha <sneha.varshney_cs19@gla.ac.in>
2024-07-30 23:13:33 +0530

    Nested Route

---
+++ about-roting/src/app/blog/first/page.js
@@ -0,0 +1,8 @@
+export default function FirstBlog() {
+  return (
+    <div className="text-center">
+      <h1>Hello from First Blog</h1>
+      <p>Example of Nested Routes</p>
+    </div>
+  );
+}
