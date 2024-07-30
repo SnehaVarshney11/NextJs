28b1f4a369c52affab6cd43eeb9842648b7fd963
sneha <sneha.varshney_cs19@gla.ac.in>
2024-07-30 23:16:59 +0530

    Dynamic Route

--- about-roting/src/app/dynamic/[prodId]/page.js
+++ about-roting/src/app/dynamic/[prodId]/page.js
@@ -1,3 +1,3 @@
 export default function ProductDetails({ params }) {
-  return <h1>Details about product {params.prodId}</h1>;
+  return <h1>Details about Product {params.prodId}</h1>;
 }
---
+++ about-roting/src/app/dynamic/page.js
@@ -0,0 +1,11 @@
+export default function Dynamic() {
+  return (
+    <div className="text-center">
+      <h1>Hello from dynamic</h1>
+      <p>Example of Dynamic Routes</p>
+      <p>Product 1</p>
+      <p>Product 2</p>
+      <p>Product 3</p>
+    </div>
+  );
+}
