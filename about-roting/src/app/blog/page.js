e04e2673b968bea5ff5c9315bc675f9acbbd4774
sneha <sneha.varshney_cs19@gla.ac.in>
2024-09-04 12:49:45 +0530

    deleted

--- about-roting/src/app/blog/page.js
+++
@@ -1,8 +0,0 @@
-export default function Blog() {
-  return (
-    <div className="text-center">
-      <h1>Hello from Blog</h1>
-      <p>Example of Nested Routes</p>
-    </div>
-  );
-}
--- about-roting/src/app/blog/second/page.js
+++
@@ -1,8 +0,0 @@
-export default function SecondBlog() {
-  return (
-    <div className="text-center">
-      <h1>Hello from Second Blog</h1>
-      <p>Example of Nested Routes</p>
-    </div>
-  );
-}
