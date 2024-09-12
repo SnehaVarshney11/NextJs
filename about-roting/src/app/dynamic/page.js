8dc028db1317e81405b5ac65243bf411601fd6b9
sneha <sneha.varshney_cs19@gla.ac.in>
2024-09-12 13:34:34 +0530

    Link Component

--- about-roting/src/app/dynamic/page.js
+++ about-roting/src/app/dynamic/page.js
@@ -1,11 +1,26 @@
+import Link from "next/link";
+
 export default function Dynamic() {
+  const prodId = 100;
   return (
     <div className="text-center">
+      <Link href="/">Home</Link>
       <h1>Hello from dynamic</h1>
       <p>Example of Dynamic Routes</p>
-      <p>Product 1</p>
-      <p>Product 2</p>
-      <p>Product 3</p>
+      <h2>
+        <Link href="dynamic/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="dynamic/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="dynamic/3" replace>
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`dynamic/${prodId}`}>Product {prodId}</Link>
+      </h2>
     </div>
   );
 }
--- about-roting/src/app/page.js
+++ about-roting/src/app/page.js
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="text-center">
-      <h>Hello from Home Page</h>
-      <p> Example of Roting</p>
+      <h1>Hello from Home Page</h1>
+      <p>Example of Routing</p>
+      <Link href="/dashboard" className="block">
+        Dashboard
+      </Link>
+      <Link href="/dynamic" className="block">
+        Products
+      </Link>
     </div>
   );
 }
